8bb46d52ea8ecc2151e20d65dd5653856a8c2b8d
Mitchell Rodio <56127734+MitchellRodio@users.noreply.github.com>
2020-11-13 10:42:44 -0700

    Add LunchAccount.java

---
+++ LunchAccount.java
@@ -0,0 +1,46 @@
+public class lunchAccount {
+    private String student;
+    public int id;
+    private double balence;
+ 
+    
+    
+    public String getStudent() {
+        return this.student;
+    }
+    
+    public int getId() {
+    	return this.id;
+    
+    }
+    
+    public double balence() {
+    	return this.balence; 
+    }
+ 
+    
+    
+    
+    public void setStudent(String student) {
+        this.student = student;
+    }
+    
+    public void setId(int id) {
+    	this.id = id;
+    }
+    
+    public void setBalence(double balence) {
+    	this.balence = balence; 
+    	
+    }
+    
+   // public static void printData(lunchAccount) {
+      //  Syetem.out.print(this.student)
+        // System.out.print(:)
+        // System.out.print(this.id)
+        // System.out.print(:)
+        // System.out.print(this.balance)
+
+    }
+    
+}
