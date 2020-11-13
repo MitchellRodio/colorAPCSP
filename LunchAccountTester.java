1fde463ca717a93c92608cedce12680af3eacde6
Mitchell Rodio <56127734+MitchellRodio@users.noreply.github.com>
2020-11-13 10:43:10 -0700

    Add LunchAccountTester.java

---
+++ LunchAccountTester.java
@@ -0,0 +1,15 @@
+class LunchAccountTester {
+    public static void main(String[] args) {
+        
+    	lunchAccount obj = new lunchAccount();
+    	 
+    	obj.setId(1);  // OK
+    	obj.setStudent("Harrold");
+    	obj.setBalence(30.2);
+    	
+    	System.out.println("" + id);
+    	
+    	
+    	
+    }
+}
